cfe4f3ddc5c821dee5a6cc18f72c090e0433337d
John Marsden <john.marsden24@gmail.com>
2020-02-21 16:56:53 +0000

    Landing page - sorta done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,9 @@
 class Cocktail < ApplicationRecord
+  has_many_attached :photos
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
+  validates :photos, presence: true
 end
