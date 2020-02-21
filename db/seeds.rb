fc3837d63ea99e3383d158b1607ad2e6fea8946d
John Marsden <john.marsden24@gmail.com>
2020-02-21 12:05:41 +0000

    another

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "open-uri"
 require "json"
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 content = JSON.parse(url)
 
 content["drinks"].each do |ingredient|
