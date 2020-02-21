0521adaeba97a75711aa4ce6d0d0222b9c9a0755
John Marsden <john.marsden24@gmail.com>
2020-02-21 12:16:11 +0000

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 require "open-uri"
 require "json"
 
+puts "Ingredients are being deleted"
+
+Ingredient.destroy_all
+
+puts "Ingredients are being created"
+
 url = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 content = JSON.parse(url)
 
@@ -8,4 +14,4 @@ content["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
 
-
+puts "#{Ingredient.count} ingredients have been made"
